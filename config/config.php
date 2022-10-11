d0e306ec6e05741dcf916f7ac1fcf30b89a7abb8
jpennyiii <201880068@psu.palawan.edu.ph>
2022-10-11 10:49:28 +0800

    config done. Readme done

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 	define('ROOT_URL', 'guestbook-login.php');
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', '');
-	define('DB_NAME', 'logbook');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6525660');
+	define('DB_PASS', 'WSVSxcWPNC');
+	define('DB_NAME', 'sql6525660');
